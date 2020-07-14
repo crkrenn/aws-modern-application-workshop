394c3388f3cd9a6d599d15df602addc2213d1bfc
EC2 Default User <ec2-user@ip-172-31-14-0.us-west-2.compute.internal>
2020-07-14 03:36:22 +0000

    url

--- bash_variables.sh
+++ bash_variables.sh
@@ -2,3 +2,5 @@ export REPLACE_ME_BUCKET_NAME=dgps-m-mysfit
 export REPLACE_ME_YOUR_REGION=us-west-2
 
 git config credential.helper 'cache --timeout=3600'
+
+echo http://$REPLACE_ME_BUCKET_NAME.s3-website-$REPLACE_ME_YOUR_REGION.amazonaws.com
