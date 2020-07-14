5c82983764c5a28735795b1b8d6a2b1d87e06e95
EC2 Default User <ec2-user@ip-172-31-14-0.us-west-2.compute.internal>
2020-07-14 03:50:30 +0000

    while true loop

--- bash_variables.sh
+++ bash_variables.sh
@@ -4,3 +4,4 @@ export REPLACE_ME_YOUR_REGION=us-west-2
 git config credential.helper 'cache --timeout=3600'
 
 echo http://$REPLACE_ME_BUCKET_NAME.s3-website-$REPLACE_ME_YOUR_REGION.amazonaws.com
+# while [ 1 ]; do aws cloudformation describe-stacks --stack-name MythicalMysfitsCoreStack | grep -i -e status; sleep 5; done
